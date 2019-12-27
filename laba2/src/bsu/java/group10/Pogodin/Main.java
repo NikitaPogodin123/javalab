f0462658657c721c4ea94a3b370fb04731a51ef1
NikitaPogodin <nik.pogodzin@mail.ru>
2019-12-27 17:06:31 +0300

    add lab_2

---
+++ laba2/src/bsu/java/group10/Pogodin/Main.java
@@ -0,0 +1,14 @@
+package bsu.java.group10.Pogodin;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		formula frame = new formula();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
